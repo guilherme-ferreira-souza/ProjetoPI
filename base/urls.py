754c3d8a96fd1c424279bcda1bc821eaf396f3e8
guilherme-ferreira-souza <80338511+guilherme-ferreira-souza@users.noreply.github.com>
2022-11-17 10:27:25 -0300

    template base pronto (add {%static%} nas imagens)

--- base/urls.py
+++ base/urls.py
@@ -1,18 +1,9 @@
 from django.urls import path
-from .views import PerfilView, CadView, LoginView, PedidosView, BaseView, CarView, EuroView, FavView, RetroView, SacView, SelecView, TesteView, UserCreate
+from .views import IndexView, BaseView
 
 urlpatterns = [
-    path('cadastro/', UserCreate.as_view(), name = 'cadastro'),
-    path('perfil/', PerfilView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('pedidos/', PedidosView.as_view()),
-    path('', BaseView.as_view(), name = 'home'),
-    path('torcedor/', BaseView.as_view(), name = 'torcedor'),
-    path('carrinho/', CarView.as_view()),
-    path('europeias/', EuroView.as_view(), name = 'euro'),
-    path('favoritos/', FavView.as_view()),
-    path('retro/', RetroView.as_view(), name = 'retro'),
-    path('sac/', SacView.as_view()),
-    path('selecoes/', SelecView.as_view(), name = 'selec'),
-    path('teste/', TesteView.as_view(), name = 'teste'),
+    path('', IndexView.as_view(), name = 'home'),
+    path('teste/', BaseView.as_view(), name = 'teste'),
+
+
 ]
--- base/views.py
+++ base/views.py
@@ -11,77 +11,8 @@ from django.contrib.auth.decorators import login_required
 
 from .models import User
 
-# Create your views here.
-'''
-def view_home(request):
-    return httpResponse('pag')
-
-def view_about(request):
-    return httpResponse('about')
-'''
-
-
-
-
-'''def view_home(request):
-    return render (request, 'home.html')'''
-  
-
-class LoginView(TemplateView):
-    template_name = 'base/pages/login.html'
-
-class PerfilView(TemplateView):
-    template_name = 'base/pages/perfil.html'
-
-class PedidosView(TemplateView):
-    template_name = 'base/pages/pedidos.html'
+class IndexView(TemplateView):
+    template_name = 'base/index.html'
 
 class BaseView(TemplateView):
-    template_name = 'base/pages/home.html'
-
-class CadView(TemplateView):
-    template_name = 'base/pages/cadastro.html'
-
-class CarView(TemplateView):
-    template_name = 'base/pages/carrinho.html'
-
-class EuroView(TemplateView):
-    template_name = 'base/pages/europa.html'
-
-class FavView(TemplateView):
-    template_name = 'base/pages/favoritos.html'
-
-class RetroView(TemplateView):
-    template_name = 'base/pages/retro.html'
-
-class SacView(TemplateView):
-    template_name = 'base/pages/sac.html'
-
-class SelecView(TemplateView):
-    template_name = 'base/pages/selecoes.html'
-
-class TesteView(TemplateView):
     template_name = 'teste.html'
-    
-#def register(request):
- #   if request.method == 'GET':
-  #      return render(request, "base/pages/cadastro.html")
-    
-   # else:
-    #    username = request.POST.get('username')
-     #   email = request.POST.get('email')
-      #  password = request.POST.get('password')
-        
-       # user = User.objects.filter(username = username).first()   
-        #if user:
-         #   return render(request, "core/pages/permission.html")
-        
-        #user = User.objects.create_user(username=username, email=email, password=password) 
-        #user.save()
-        #return render(request, "base/pages/home.html")
-
-class UserCreate(CreateView):
-    model = User
-    fields = ['email','password']
-    template_name = 'base/cadastro.html'
-    success_url = reverse_lazy('home.html')
